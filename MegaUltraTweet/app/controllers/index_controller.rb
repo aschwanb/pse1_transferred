c83c2ed3e2a53503d1a37e47e6235710536bbd78
aschwanb <balz.aschwanden@students.unibe.ch>
2015-03-08 19:28:40 +0100

    Added default search page to index

--- MegaUltraTweet/app/controllers/index_controller.rb
+++ MegaUltraTweet/app/controllers/index_controller.rb
@@ -3,6 +3,9 @@ class IndexController < ApplicationController
   def start
   end
 
+  def search
+  end
+
   def hashtag
   end
 
@@ -16,3 +19,14 @@ class IndexController < ApplicationController
   end
 end
 
+require 'rubygems'
+require 'twitter'
+require 'uri'
+
+# Client needs to be authenticated in order to access twitter api
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "tTrNPGMT8S1d3qK3LMlnZV1XP"
+  config.consumer_secret     = "olUHmGtYlh6dx3ztWqa6ExLLek7Vb76vGEi5p5BMd2LiFWWHPD"
+  config.access_token        = "3062227378-HaWeilWyykpsDQvwZmaGUUSHDmlOFlHxpHpC9RY"
+  config.access_token_secret = "etP0a6eCI0q1FwfMJYUO0VsTWyrhbYKRvuvUS8YKH2kC3"
+end
--- MegaUltraTweet/test/controllers/index_controller_test.rb
+++ MegaUltraTweet/test/controllers/index_controller_test.rb
@@ -25,4 +25,9 @@ class IndexControllerTest < ActionController::TestCase
     get :about
     assert_response :success
   end
+
+  test "should get search" do
+    get :search
+    assert_response :success
+  end
 end
