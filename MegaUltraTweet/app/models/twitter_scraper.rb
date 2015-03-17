683a4cdd788c65c950238281ae0d2b8e355276bb
aschwanb <balz.aschwanden@students.unibe.ch>
2015-03-17 22:34:59 +0100

    Scraper 0.1 done

--- MegaUltraTweet/app/models/twitter_scraper.rb
+++ MegaUltraTweet/app/models/twitter_scraper.rb
@@ -9,28 +9,28 @@ class TwitterScraper
 
   def scrape(query, querySize, depth, detail)
     puts "Depth is at #{depth}"
-    p depth
-    puts depth.to_f.class
-    while depth.to_f > 0
-      twitterClient = TwitterClient.new
-      tmpQuery = query.clone
-      while tmpQuery.any? do
-        localQuery = tmpQuery.pop
-        puts "Scraping for #{localQuery}"
-        twitterClient.search(localQuery, querySize)
-      end
-      # All tweets are saved in twitterClient.getTweets
-      # Get all new hashtags without the ones present in the last query
-      newQuery = twitterClient.getHashtagsAsHash
-      query.each { |t| newQuery.delete(t.downcase) }
-      puts newQuery
-      # Determine how many of them to take
-      newQuery = newQuery.first(detail).map(&:first).to_a
-      # Start a new search with one less depth
-      scrape(newQuery, querySize, depth-1, detail)
-      # Do not exceed query limit set by twitter
-      # TODO
+    twitterClient = TwitterClient.new
+    tmpQuery = query.clone
+    while tmpQuery.any? do
+      localQuery = tmpQuery.pop
+      puts "Scraping for #{localQuery}"
+      twitterClient.search(localQuery, querySize)
     end
+    # All tweets are saved in twitterClient.getTweets
+    # Get all new hashtags without the ones present in the last query
+    newQuery = twitterClient.getHashtagsAsHash
+    query.each { |t| newQuery.delete(t.downcase) }
+    puts newQuery
+    # Determine how many of them to take
+    newQuery = newQuery.first(detail).map(&:first).to_a
+    # Start a new search with one less depth
+    while depth > 1
+       depth = depth - 1
+       puts "Start new branch with #{newQuery}"
+       scrape(newQuery, querySize, depth, detail)
+    end
+    # Do not exceed query limit set by twitter
+    # TODO
   end
 
 
@@ -39,4 +39,4 @@ end
 
 topics = TopicsStart.new.getTopics
 twitterScraper = TwitterScraper.new
-twitterScraper.scrape(%w[#Technology #iWatch #Trending], 3, 2, 3)
+twitterScraper.scrape(%w[#Technology #iWatch #Trending], 2, 5, 3)
