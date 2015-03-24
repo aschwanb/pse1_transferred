1ad44a5d0379161c199b520bdfd9926500b5821e
aschwanb <balz.aschwanden@students.unibe.ch>
2015-03-24 23:01:47 +0100

    Added some docu to schedule

--- MegaUltraTweet/config/schedule.rb
+++ MegaUltraTweet/config/schedule.rb
@@ -1,3 +1,5 @@
+# Update crontab: "whenever --update-crontab MegaUltraTweet"
+# Check crontab: "crontab -l"
 
 # define environment
 root = File.expand_path(File.dirname(File.dirname(__FILE__)))
@@ -5,7 +7,7 @@ set :environment, "development"
 set :output, "%s/tmp/cron.log" % [ root ]
 
 # Define cron jobs
-every 5.minutes do
+every 15.minutes do
   command '/usr/bin/date'
   command '/usr/bin/echo "Running cron job for TwitterScraper"'
   runner "require '%s/lib/tasks/twitter_scraper.rb'; twitterScraper = TwitterScraper.new; twitterScraper.scrape( [\"#Technology\", \"#iWatch\", \"#Trending\"], 2, 5, 3)" % [ root ]
