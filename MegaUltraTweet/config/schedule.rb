b9209a9e7c8c67b10890d35f928512a8ecffc988
aschwanb <balz.aschwanden@students.unibe.ch>
2015-03-24 22:58:09 +0100

    Cron job is now working

--- MegaUltraTweet/config/schedule.rb
+++ MegaUltraTweet/config/schedule.rb
@@ -1,26 +1,12 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+# define environment
+root = File.expand_path(File.dirname(File.dirname(__FILE__)))
+set :environment, "development"
+set :output, "%s/tmp/cron.log" % [ root ]
 
-# Learn more: http://github.com/javan/whenever
-
-set :output, "/tmp/cron_log.log"
-
-every 10.minutes do
-  runner TwitterScraper.new.scrape(%w[#Technology #iWatch #Trending], 2, 5, 3)
+# Define cron jobs
+every 5.minutes do
+  command '/usr/bin/date'
+  command '/usr/bin/echo "Running cron job for TwitterScraper"'
+  runner "require '%s/lib/tasks/twitter_scraper.rb'; twitterScraper = TwitterScraper.new; twitterScraper.scrape( [\"#Technology\", \"#iWatch\", \"#Trending\"], 2, 5, 3)" % [ root ]
 end
