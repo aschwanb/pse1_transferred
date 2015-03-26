565f0b109b74c5f03135cb5a7b72391840cc3261
aschwanb <balz.aschwanden@students.unibe.ch>
2015-03-26 21:34:31 +0100

    Parsing methods for individual tweets

--- MegaUltraTweet/app/models/tweet_parser.rb
+++ MegaUltraTweet/app/models/tweet_parser.rb
@@ -30,4 +30,26 @@ class TweetParser
     return tmp
   end
 
+  def parseHashtags(tweet)
+    return tweet.text.downcase.scan(/#\w+/).flatten
+  end
+
+  def parseTwitterHandles(tweet)
+    return tweet.text.downcase.scan(/@\w+/).flatten
+  end
+
+  def parseURLs(tweet)
+    tmp = tmp + URI.extract("#{tweet.text}", /http|https/)
+    if !(tmp.nil? or tmp.empty?) and !tmp.last.match(/[[:alnum:]]$/) #regex: last char is alphabetic or numeric
+      tmp.pop
+    end
+    # Eliminate urls that are to short
+    tmp.each { |url| tmp.delete(url) if url.length < 10 }
+    return tmp
+  end
+
+  def getAuthor(tweet)
+    return Author.new(name: tweet.user.name)
+  end
+
 end
