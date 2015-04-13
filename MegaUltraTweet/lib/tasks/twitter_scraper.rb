d1259862bc69386f79acbf6d7a94cebf7ace6c0d
aschwanb <balz.aschwanden@students.unibe.ch>
2015-04-13 17:06:16 +0200

    Refactoring: Scraper done. Schedule file updated.

--- MegaUltraTweet/lib/tasks/twitter_scraper.rb
+++ MegaUltraTweet/lib/tasks/twitter_scraper.rb
@@ -7,7 +7,7 @@ class TwitterScraper
   def initialize(query_size, detail)
     @client = TwitterClient.new
     @parser = TweetParser.new
-    @provided_searches = 450
+    @provided_searches = 300  # Add some buffer for user input. Max searches is at 450
     @used_searches = 0
     @query_size = query_size  # How many results do we request from twitter
     @detail = detail  # How many of the most relevant keywords do we follow up on ?
@@ -18,36 +18,32 @@ class TwitterScraper
     Array(query).each { |keyword| scrape(keyword, depth) }
   end
 
-  # Scraping for a keyword
+  # Scraping for a keyword. Start a new searches with one less depth
   def scrape(keyword, depth)
-    # puts "Depth is at #{depth}"
+    tweets = run_query(keyword)
+    tweets.each { |t| @client.save_tweet(t) }
+    new_query = self.get_new_query(keyword, tweets)
+    depth -= 1
     if depth > 0
-      tweets = run_query(keyword)
-      tweets.each { |t| @client.save_tweet(t) }
-      new_query = self.get_new_query(keyword, tweets)
-      # Start a new search with one less depth
-      depth -= 1
-      puts "Start new branch with #{new_query}"
+      puts "Finished with #{keyword}. Start new branch with #{new_query}"
       start(new_query, depth)
-      else
-      puts "Finished scraping for now"
     end
-  rescue Exceptions::MaxSearchesError => e
+  rescue Exception::MaxSearchesError => e
+    puts e.message
+  rescue Twitter::Error => e
     puts e.message
   end
 
   def run_query(keyword)
-    puts "Scraping for #{keyword} ..."
+    puts "Scraping for \"#{keyword}\" ..."
     if @provided_searches == @used_searches
       raise MaxSearchesError.new, "Maximum searches for this time window used."
     end
     @used_searches = @used_searches + 1
-    puts "Start"
-    return @client.search_simple(keyword, @querySize)
+    @client.search_simple(keyword, @query_size)
   end
 
-  # Returns the "detail" most relevant keywords
-  # without the one present in the last query
+  # Returns the "detail" most relevant keywords - without the one present in the last query
   def get_new_query(keyword, tweets)
     new_query = @client.get_hashtags_to_h(tweets)
     new_query.delete(keyword.downcase)
