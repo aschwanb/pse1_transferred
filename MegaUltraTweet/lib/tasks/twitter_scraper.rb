4ea2778b3028ed3da60e6d5813fd447e5c9f7125
aschwanb <balz.aschwanden@students.unibe.ch>
2015-04-13 16:10:50 +0200

    Added custom error for maximum searches used

--- MegaUltraTweet/lib/tasks/twitter_scraper.rb
+++ MegaUltraTweet/lib/tasks/twitter_scraper.rb
@@ -2,80 +2,56 @@ require_relative '../../app/models/twitter_client'
 require_relative '../../app/models/tweet'
 require_relative '../../app/models/tweet_parser'
 
-# TODO: Run a new search the most popular (depth) hashtags for one entry
 class TwitterScraper
 
-  def initialize
+  def initialize(query_size, detail)
     @client = TwitterClient.new
     @parser = TweetParser.new
     @provided_searches = 450
     @used_searches = 0
+    @query_size = query_size  # How many results do we request from twitter
+    @detail = detail  # How many of the most relevant keywords do we follow up on ?
   end
 
   # Scraping for a array of key words
-  def scrape(query, query_size, depth, detail)
+  def start(query, depth)
+    Array(query).each { |keyword| scrape(keyword, depth) }
+  end
+
+  # Scraping for a keyword
+  def scrape(keyword, depth)
     # puts "Depth is at #{depth}"
     if depth > 0
-      begin
-        self.run_query(query, query_size)
-        new_query = self.get_new_query(query, detail)
-        self.save_and_reset
-        # Start a new search with one less depth
-        depth = depth - 1
-        puts "Start new branch with #{new_query}"
-        scrape(new_query, query_size, depth, detail)
-      rescue
-        puts "Maximum searches for this time window used."
-      end
-    else
+      tweets = run_query(keyword)
+      tweets.each { |t| @client.save_tweet(t) }
+      new_query = self.get_new_query(keyword, tweets)
+      # Start a new search with one less depth
+      depth -= 1
+      puts "Start new branch with #{new_query}"
+      start(new_query, depth)
+      else
       puts "Finished scraping for now"
     end
+  rescue Exceptions::MaxSearchesError => e
+    puts e.message
   end
 
-  # TODO: Scraper for individual key words
-  # As before, but take the n most popular hashtags for each key word
-
-
-
-  def run_query(query, querySize)
-    while query.any? do
-      local_query = query.pop
-      puts "Scraping for #{local_query} ..."
-      @client.search_simple(local_query, querySize)
-      @used_searches = @used_searches + 1
-      puts "Used searches #{@used_searches}"
-      puts "Provided searches #{@provided_searches}"
-      if @provided_searches <= @used_searches
-        raise
-      end
+  def run_query(keyword)
+    puts "Scraping for #{keyword} ..."
+    if @provided_searches == @used_searches
+      raise MaxSearchesError.new, "Maximum searches for this time window used."
     end
+    @used_searches = @used_searches + 1
+    puts "Start"
+    return @client.search_simple(keyword, @querySize)
   end
 
-  def get_new_query(query, detail)
-    # Get all new hashtags without the ones present in the last query
-    new_query = @client.get_hashtags_to_h
-    query.each { |t| new_query.delete(t.downcase) }
-    # Determine how many of them to take
-    new_query = new_query.first(detail).map(&:first).to_a
-    return new_query
-  end
-
-  def save_and_reset
-    @client.get_tweets_to_a.each { |t| save_tweet(t) }
-    @client.reset_tweets
-  end
-  # TODO: Move to TwitterClient
-  def save_tweet(tweet)
-     author = @parser.get_author(tweet)
-     if Tweet.where(twitter_id: tweet.id).blank?
-        t = author.tweets.create(
-          text: tweet.text,
-          retweets: tweet.retweet_count,
-          twitter_id: tweet.id
-        )
-      t.set_hashtags(@parser.parse_hashtags(tweet))
-      t.set_webpages(@parser.parse_webpages(tweet))
-    end
+  # Returns the "detail" most relevant keywords
+  # without the one present in the last query
+  def get_new_query(keyword, tweets)
+    new_query = @client.get_hashtags_to_h(tweets)
+    new_query.delete(keyword.downcase)
+    return new_query.first(@detail).map(&:first).to_a
   end
 
 end
