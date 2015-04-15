0e2063de4608b832df5f518b0aac30f9ead536f6
aschwanb <balz.aschwanden@students.unibe.ch>
2015-04-15 09:10:53 +0200

    Create hashtag pairs WIP

--- MegaUltraTweet/app/models/hashtag_pair.rb
+++ MegaUltraTweet/app/models/hashtag_pair.rb
@@ -1,4 +1,13 @@
 class HashtagPair < ActiveRecord::Base
   belongs_to :hashtag_first, class_name: :Hashtag, foreign_key: :hashtag_first_id
   belongs_to :hashtag_second, class_name: :Hashtag, foreign_key: :hashtag_second_id
+
+  def get_popularity_now
+    return self.popularity_now
+  end
+
+  def get_popularity_old
+    return self.populairity_old
+  end
+
 end
--- MegaUltraTweet/app/models/tweet.rb
+++ MegaUltraTweet/app/models/tweet.rb
@@ -25,7 +25,24 @@ class Tweet < ActiveRecord::Base
       end
       # TODO: In implement downvoting
       # TODO: Move logic somewhere els
-      # TODO: Create hashtag pairs and update their popularity
+      # Loop through all combinations of pairs in hashtag array
+      case hashtag_two
+        when HashtagPair.where(hashtag_first: hashtag, hashtag_second: hashtag_two).blank? && HashtagPair.where(hashtag_first: hashtag_two, hashtag_second: hashtag).blank?
+          # Pair does not exist
+          pair = HashtagPair.create(
+                         hashtag_first: hashtag,
+                         hashtag_second: hasthatag_two,
+                         popularity_now: 0,
+                         popularity_old: 0
+          )
+        when !HashtagPair.where(hashtag_first: hashtag, hashtag_second: hashtag_two).blank?
+          # Pair exists. Update Popularity
+          pair = HashtagPair.find_by(hashtag_first: hashtag, hashtag_second: hashtag_two)
+        when !HashtagPair.where(hashtag_first: hashtag_two, hashtag_second: hashtag).blank?
+          # Pair exists. Update Popularity
+          pair = HashtagPair.find_by(hashtag_first: hashtag_two, hashtag_second: hashtag)
+      end
+      pair.update(popularity_now: pair.get_popularity_now + 1)
 
     end
   end
