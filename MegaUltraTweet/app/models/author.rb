96a05aad3f3aed97f234f6319d1227597115c32b
aschwanb <balz.aschwanden@students.unibe.ch>
2015-04-15 21:38:29 +0200

    Author object is now updated. Alwasy up to date.

--- MegaUltraTweet/app/models/author.rb
+++ MegaUltraTweet/app/models/author.rb
@@ -3,6 +3,25 @@ class Author < ActiveRecord::Base
 
   scope :by_screen_name, -> (screen_name) { where screen_name: screen_name}
 
+  def update_all(name, friends_count, followers_count, screen_name)
+    if self.name != name
+      self.update(name: name)
+    end
+
+    if self.friends_count != friends_count
+      self.update(friends_count: friends_count)
+    end
+
+    if self.followers_count != followers_count
+      self.update(followers_count: followers_count)
+    end
+
+    if self.screen_name != screen_name
+      self.update(screen_name: screen_name)
+    end
+
+  end
+
   def get_name
     return self.name
   end
