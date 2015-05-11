10c6b198e95f389fbf8d692f9e30f15df54a77fa
aschwanb <balz.aschwanden@students.unibe.ch>
2015-05-11 16:28:20 +0200

    Hashtags without valide tweets are ignored when building new trending object

--- MegaUltraTweet/app/models/trending.rb
+++ MegaUltraTweet/app/models/trending.rb
@@ -11,6 +11,15 @@ has_and_belongs_to_many :hashtags
     end
   end
 
+  def get_hashtags_with_tweets(hashtag_array)
+    Array(hashtag_array).each do |hashtag|
+      if !hashtag.get_tweets.any?
+        hashtag_array.delete(hashtag)
+      end
+    end
+    return hashtag_array
+  end
+
 # Short and long intervals are defined in MegaUltraTweet::Application
 # The short/long functions are similar but take advantage of the different intervals
 # It is recommended to use two different objects for the two intervals (see seeds.rb)
@@ -26,11 +35,12 @@ has_and_belongs_to_many :hashtags
   end
 
   # Clear hashtags for this object and at the ones that have been used the most/least in the given interval: short
+  # Hashtags are only added if they have tweets. (Relevant for small databases)
   def build_new_short
     hashtags = Hashtag.all
     hashtags = hashtags.sort_by{ |hashtag| hashtag.get_trend_short }.reverse
-    top = hashtags.first(MegaUltraTweet::Application::TRENDING_HASHTAGS_NUMBER)
-    bottom = hashtags.last(MegaUltraTweet::Application::TRENDING_HASHTAGS_NUMBER)
+    top = self.get_hashtags_with_tweets(hashtags).first(MegaUltraTweet::Application::TRENDING_HASHTAGS_NUMBER)
+    bottom = self.get_hashtags_with_tweets(hashtags).last(MegaUltraTweet::Application::TRENDING_HASHTAGS_NUMBER)
     reset_hashtags
     set_hashtags(top)
     set_hashtags(bottom)
@@ -47,11 +57,12 @@ has_and_belongs_to_many :hashtags
   end
 
 # Clear hashtags for this object and at the ones that have been used the most/least in the given interval: long
+# Hashtags are only added if they have tweets. (Relevant for small databases)
   def build_new_long
     hashtags = Hashtag.all
     hashtags = hashtags.sort_by{ |hashtag| hashtag.get_trend_long }.reverse
-    top = hashtags.first(MegaUltraTweet::Application::TRENDING_HASHTAGS_NUMBER)
-    bottom = hashtags.last(MegaUltraTweet::Application::TRENDING_HASHTAGS_NUMBER)
+    top = self.get_hashtags_with_tweets(hashtags).first(MegaUltraTweet::Application::TRENDING_HASHTAGS_NUMBER)
+    bottom = self.get_hashtags_with_tweets(hashtags).last(MegaUltraTweet::Application::TRENDING_HASHTAGS_NUMBER)
     reset_hashtags
     set_hashtags(top)
     set_hashtags(bottom)
