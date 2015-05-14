b67547a5a381179170b68125e7f33ca39aef5fdc
luoar <ralle23091993@gmail.comssh-keygen -t rsa -C ralle23091993@gmail.com>
2015-05-14 12:17:49 +0200

    removed wrong test for id

--- MegaUltraTweet/app/models/author_hashtag.rb
+++ MegaUltraTweet/app/models/author_hashtag.rb
@@ -3,9 +3,7 @@ class AuthorHashtag < ActiveRecord::Base
   belongs_to :hashtag
   has_one :popularity, as: :popular
 
-  # TODO: Validation fails if standard Object.create function is used.
-  # Fix validation to validate after creation before inserting into db?
-  # validates :id, :author_id, :hashtag_id, presence: true
+ validates :author_id, :hashtag_id, presence: true
 
   def get_rank
     self.popularity.get_times_used
--- MegaUltraTweet/spec/models/author_hashtag_spec.rb
+++ MegaUltraTweet/spec/models/author_hashtag_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe AuthorHashtag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe AuthorHashtag do
+
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:author_hashtag)).to be_valid
+  end
+
+  it "is invalid without a author_id" do
+    expect(FactoryGirl.build(:author_hashtag, author_id: nil)).not_to be_valid
+  end
+
+  it "is invalid without a hashtag_id" do
+    expect(FactoryGirl.build(:author_hashtag, hashtag_id: nil)).not_to be_valid
+  end
+
+  describe "#get_rank" do
+    it "returns the rank of the author_hashtag"
+  end
+
+  describe "#set_rank" do
+    it "sets the rank of the author_hashtag"
+  end
+
+  describe "#get_trend_short" do
+    it "returns the trend_short of the author_hashtag"
+  end
+
+  describe "#get_trend_long" do
+    it "returns the trend_long of the author_hashtag"
+  end
+
+  describe "#get_new_short?" do
+    it "returns the new_short of the author_hashtag"
+  end
+
+  describe "#set_new_short" do
+    it "sets the new_short of the author_hashtag"
+  end
+
+  describe "#get_new_long?" do
+    it "returns the new_long of the author_hashtag"
+  end
+
+  describe "#set_new_long" do
+    it "sets the new_long of the author_hashtag"
+  end
+
 end
